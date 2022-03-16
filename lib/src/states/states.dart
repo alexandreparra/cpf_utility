0434c0ed4826da1fe632147dc37c91bddc6656a6
Alexandre Parra <duffhd@protonmail.com>
2022-03-16 13:10:14 -0300

    refactor: moved States enum to this file

---
+++ lib/src/states/states.dart
@@ -0,0 +1,13 @@
+/// [States] represents the official 26 state codes of Brazil.
+enum States {
+  df, go, ms, to,         // 1
+  pa, am, ac, ap, ro, rr, // 2
+  ce, ma, pi,             // 3
+  pe, rn, pb, al,         // 4
+  ba, se,                 // 5
+  mg,                     // 6
+  rj, es,                 // 7
+  sp,                     // 8
+  pr, sc,                 // 9
+  rs                      // 0
+}
