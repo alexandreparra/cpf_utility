489d939afffede56f449793cba437176907bef16
Alexandre Parra <duffhd@protonmail.com>
2022-03-16 20:37:38 -0300

    feat: updated library docstring

--- lib/cpf_utility.dart
+++ lib/cpf_utility.dart
@@ -1,9 +1,9 @@
-/// Generate or validate Brazilian CPF's.
+/// Generate, validate and format Brazilian CPF's.
 ///
-/// Your can generate a random valid CPF or select a state
-/// to which the CPF will be generated.
-///
-/// And you can validate a given CPF.
+/// cpf_utility is a simple library where you can:
+/// - Generate a CPF from a Brazilian state or not (in this case, the state will be random).
+/// - Validate a formatted or unformatted CPF.
+/// - Format or strip the format of a CPF.
 library cpf_utility;
 
 export 'src/states/states.dart';
