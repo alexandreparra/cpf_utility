c47e603d64c5a3f3b667362da452a97cff15bebd
Alexandre Parra <duffhd@protonmail.com>
2022-03-16 13:06:53 -0300

    removed old unused files

--- lib/src/extension.dart
+++
@@ -1,7 +0,0 @@
-extension StringToArray on String {
-  /// Grabs the first 9 digits of the CPF which are the ones
-  /// used to validate it.
-  List<int> toCpfValidation() {
-    return List.generate(9, (i) => int.parse(this[i])).toList();
-  }
-}
