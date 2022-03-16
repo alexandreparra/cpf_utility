35829a7d6582a87d725c71e90ba79160b7314b58
Alexandre Parra <duffhd@protonmail.com>
2022-03-16 13:06:15 -0300

    feat: added CpfGenerator class namespaces and static functions

---
+++ lib/src/cpf/cpf_generator.dart
@@ -0,0 +1,33 @@
+/// The explanation of how the CPF creation works can be found here
+/// [Algoritmo do CPF](https://www.geradorcpf.com/algoritmo_do_cpf.htm).
+class CpfGenerator {
+  static String generate(List<int> cpf) {
+    var firstTable = List.generate(9, (i) => i+2).reversed.toList();
+    var secondTable = List.generate(10, (i) => i+2).reversed.toList();
+
+    _sumColumns(cpf, firstTable);
+    _sumColumns(cpf, secondTable);
+
+    return cpf.join();
+  }
+
+  static void _sumColumns(List<int> cpf, List<int> validationTable) {
+    var sum = 0;
+
+    for (var i = 0; i < cpf.length; i++) {
+      sum += cpf[i] * validationTable[i];
+    }
+
+    cpf.add(_magicNumber(sum));
+  }
+
+  static int _magicNumber(int columnSum) {
+    var remainder = columnSum % 11;
+
+    if (remainder < 2) {
+      return 0;
+    } else {
+      return 11 - remainder;
+    }
+  }
+}
